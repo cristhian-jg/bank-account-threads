87fcbc1676796007313107be4ffc0e47a16fe9a1
Cristhian <cristhianbay@gmail.com>
2019-12-03 22:02:21 +0100

    Creadas acciones aleatorias de los hilos

--- src/com/crisgon/Persona.java
+++ src/com/crisgon/Persona.java
@@ -2,22 +2,41 @@ package com.crisgon;
 
 public class Persona extends Thread {
 
-    private String name;
+    private String nombre;
     private Cuenta cuenta;
 
-    public Persona(String name, Cuenta cuenta) {
-        super(name);
+    public Persona(String nombre, Cuenta cuenta) {
+        super(nombre);
         this.cuenta = cuenta;
     }
 
     public void run() {
 
-        int aleatorio;
+        boolean error = false; // Controla si ha habido un error en el proceso.
 
-        while (true) {
-            aleatorio = ((int) (Math.random()*500+1));
+        int ingreso;
+        int reintegro;
+
+        while (!error) {
+
+            ingreso = ((int) (Math.random()*500+1));
+
+            if (cuenta.setIngreso(ingreso)) {
+                System.out.println("Se han ingresado: " + ingreso);
+            } else {
+                System.out.println("Se ha superado la cantidad máxima para la cuenta.");
+                error = true;
+            }
+
+            reintegro = ((int) (Math.random()*500+1));
+
+            if (cuenta.setReintegro(reintegro)) {
+                System.out.println("Se han reintegrado: " + reintegro);
+            } else {
+                System.out.println("No hay saldo disponible");
+                error = true;
+            }
         }
-
     }
 
 }
